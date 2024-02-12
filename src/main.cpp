e123da3a3d9b354e67e85c10bceffa8894bafe46
Logesh G <logeshg5@gmail.com>
2024-02-12 15:53:47 +0530

    Rename profile to config

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 class MyApp : public wxApp {
    public:
     virtual bool OnInit();
-    bool parserCmdLine(wxString& file, wxString& profile);
+    bool parserCmdLine(wxString& file, wxString& config);
 };
 
 IMPLEMENT_APP(MyApp);
@@ -16,25 +16,25 @@ IMPLEMENT_APP(MyApp);
 bool MyApp::OnInit()
 {
     auto* frame = new MainFrameImpl(nullptr, wxID_ANY, "Logalizer");
-    wxString file, profile;
-    parserCmdLine(file, profile);
-    frame->setCmdLineArgs(file, profile);
+    wxString file, config;
+    parserCmdLine(file, config);
+    frame->setCmdLineArgs(file, config);
     frame->Show(true);
     return true;
 }
 
-bool MyApp::parserCmdLine(wxString& file, wxString& profile)
+bool MyApp::parserCmdLine(wxString& file, wxString& config)
 {
     static const wxCmdLineEntryDesc cmdLineDesc[] = {
         {wxCMD_LINE_SWITCH, "h", "help", "Displays this help", wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP},
         {wxCMD_LINE_OPTION, "f", "file", "Input log file", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL},
-        {wxCMD_LINE_OPTION, "p", "profile", "Profile", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL},
+        {wxCMD_LINE_OPTION, "c", "config", "Configuration", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL},
         {wxCMD_LINE_NONE}};
 
     wxCmdLineParser parser(cmdLineDesc, argc, argv);
     parser.SetSwitchChars("-");
     parser.Parse();
     parser.Found("f", &file);
-    parser.Found("p", &profile);
+    parser.Found("c", &config);
     return true;
 }
